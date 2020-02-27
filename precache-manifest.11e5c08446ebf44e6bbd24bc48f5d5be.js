3aaa3f6e6e7a911c82fec67481b239531ecd398b
Stephanie Kerr <steph.vm.kerr@gmail.com>
2020-02-27 12:38:53 -0500

    Updates

---
+++ precache-manifest.11e5c08446ebf44e6bbd24bc48f5d5be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aada57e257126e8157a2dcb3e18e2b9c",
+    "url": "/thinkMindful/index.html"
+  },
+  {
+    "revision": "2a4aef81b5f5b09969bc",
+    "url": "/thinkMindful/static/css/main.0c6a8225.chunk.css"
+  },
+  {
+    "revision": "4a5e12a1163f129a84f0",
+    "url": "/thinkMindful/static/js/2.3849ac49.chunk.js"
+  },
+  {
+    "revision": "7f84d11092c10d3a43893d78f68c23cb",
+    "url": "/thinkMindful/static/js/2.3849ac49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a4aef81b5f5b09969bc",
+    "url": "/thinkMindful/static/js/main.51b88fd1.chunk.js"
+  },
+  {
+    "revision": "c9bfe6d8829125110f3d",
+    "url": "/thinkMindful/static/js/runtime-main.8da75973.js"
+  }
+]);
